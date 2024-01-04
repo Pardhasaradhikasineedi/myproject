aa38dcdedcdb1908ed8ee8dbc68cc9c063ecda33
pardhasaradhikasineedi <pardhuchowdary500@gmail.com>
2024-01-04 13:57:26 -0600

    first commit

---
+++ pardhu/script.js
@@ -0,0 +1,15 @@
+const form = document.querySelector("form"),
+        allInput = form.querySelectorAll(".first input");
+
+
+submit.addEventListener("click", ()=> {
+    allInput.forEach(input => {
+        if(input.value != ""){
+            form.classList.add('secActive');
+        }else{
+            form.classList.remove('secActive');
+        }
+    })
+})
+
+backBtn.addEventListener("click", () => form.classList.remove('secActive'));
